eab88f789357ed8b5d68ae97bff3537492bdd5c7
ericamendez <ecmendez25@gmail.com>
2020-01-18 12:07:13 -0500

    char and boolean

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//      char is width of 16 (2 bytes)
+        char myChar = 'E';
+        char myCharUnicode = '\u00A9';
+        System.out.println("Unicode output was: " + myCharUnicode);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        
+    }
+}
